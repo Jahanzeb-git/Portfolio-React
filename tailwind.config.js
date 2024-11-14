297b83e02ff59572340d47da0db8c935453b843c
Jahanzeb Ahmed <151543326+Jahanzeb-git@users.noreply.github.com>
2024-11-14 22:18:56 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,9 @@ export default {
   theme: {
     extend: {
       animation: {
-        'gradient-x': 'gradient-x 15s ease infinite',
-        'gradient-y': 'gradient-y 15s ease infinite',
-        'gradient-xy': 'gradient-xy 15s ease infinite',
+        'gradient-x': 'gradient-x 255s ease infinite',
+        'gradient-y': 'gradient-y 25s ease infinite',
+        'gradient-xy': 'gradient-xy 25s ease infinite',
       },
       keyframes: {
         'gradient-y': {
